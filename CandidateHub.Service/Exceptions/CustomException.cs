ee9cc29149c516f7a7b6ff854de955bca51af948
Hasanboevs1 <friday555111@gmail.com>
2025-03-01 10:59:51 +0500

    Custom Exception has beeb added

---
+++ CandidateHub.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+﻿namespace CandidateHub.Service.Exceptions;
+
+public class CustomException : Exception
+{
+    public int StatusCode { get; set; }
+
+    public CustomException(int Code, string Message) : base(Message)
+    {
+        StatusCode = Code;
+    }
+}
