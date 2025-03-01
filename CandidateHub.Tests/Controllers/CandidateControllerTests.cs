77df0126fd69542e76844f048e9303ce43d0b5b5
Hasanboevs1 <friday555111@gmail.com>
2025-03-01 15:15:53 +0500

    Tests for both Service and Controller

---
+++ CandidateHub.Tests/Controllers/CandidateControllerTests.cs
@@ -0,0 +1,211 @@
+﻿using CandidateHub.Api.Controllers;
+using CandidateHub.Service.DTOs.Candidate;
+using CandidateHub.Service.Exceptions;
+using CandidateHub.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CandidateHub.Tests.Controllers;
+
+public class CandidateControllerTests
+{
+    private readonly Mock<ICandidateService> _mockCandidateService;
+    private readonly CandidateController _controller;
+
+    public CandidateControllerTests()
+    {
+        _mockCandidateService = new Mock<ICandidateService>();
+        _controller = new CandidateController(_mockCandidateService.Object);
+    }
+
+    [Fact]
+    public async Task GetAllCandidates_ReturnsOkResult_WithCandidateList()
+    {
+        // Arrange
+        var candidates = new List<CandidateDto>
+        {
+            new CandidateDto { Id = 1, FirstName = "Leyla", LastName = "Mananov", Email = "leyla@markaz.uz" },
+            new CandidateDto { Id = 2, FirstName = "Temur", LastName = "Abdullayev", Email = "temura500@gmail.com" }
+        };
+
+        _mockCandidateService.Setup(service => service.GetAllCandidatessAsync())
+                                .ReturnsAsync(candidates);
+
+        // Act
+        var result = await _controller.GetAllCandidates();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedCandidates = Assert.IsAssignableFrom<IEnumerable<CandidateDto>>(okResult.Value);
+        Assert.Equal(2, returnedCandidates.Count());
+    }
+
+    [Fact]
+    public async Task GetCandidate_ReturnsOkResult_WithCandidate()
+    {
+        // Arrange
+        var candidateId = 1L;
+        var candidate = new CandidateDto { Id = candidateId, FirstName = "Leyle", LastName = "Mananov", Email = "leyla@markaz.uz" };
+
+        _mockCandidateService.Setup(service => service.GetCandidatetAsync(candidateId))
+                                .ReturnsAsync(candidate);
+
+        // Act
+        var result = await _controller.GetCandidate(candidateId);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedCandidate = Assert.IsType<CandidateDto>(okResult.Value);
+        Assert.Equal(candidateId, returnedCandidate.Id);
+    }
+
+
+    [Fact]
+    public async Task AddCandidate_ReturnsOkResult_WithCreatedCandidate()
+    {
+        // Arrange
+        var createDto = new CandidateCreateDto
+        {
+            FirstName = "Sardor",
+            LastName = "Murodov",
+            Email = "sardormuradov@markaz.uz"
+        };
+
+        var createdCandidate = new CandidateDto
+        {
+            Id = 1,
+            FirstName = "Sardor",
+            LastName = "Murodov",
+            Email = "sardormuradov@markaz.uz"
+        };
+
+        _mockCandidateService.Setup(service => service.CreateCandidateAsync(createDto))
+                                .ReturnsAsync(createdCandidate);
+
+        // Act
+        var result = await _controller.AddCandidateAsync(createDto);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedCandidate = Assert.IsType<CandidateDto>(okResult.Value);
+        Assert.Equal(createdCandidate.Id, returnedCandidate.Id);
+    }
+
+
+    [Fact]
+    public async Task UpdateCandidate_ReturnsOkResult_WithUpdatedCandidate()
+    {
+        // Arrange
+        var candidateId = 1L;
+        var updateDto = new CandidateUpdateDto
+        {
+            FirstName = "Sardor",
+            LastName = "Murodov",
+            Email = "sardormuradov@markaz.uz"
+        };
+
+        var updatedCandidate = new CandidateDto
+        {
+            Id = candidateId,
+            FirstName = "Sardor",
+            LastName = "Murodov",
+            Email = "sardormuradov@markaz.uz"
+        };
+
+        _mockCandidateService.Setup(service => service.UpdateCandidateAsync(candidateId, updateDto))
+                                .ReturnsAsync(updatedCandidate);
+
+        // Act
+        var result = await _controller.UpdateCandidateAsync(candidateId, updateDto);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedCandidate = Assert.IsType<CandidateDto>(okResult.Value);
+        Assert.Equal(candidateId, returnedCandidate.Id);
+    }
+
+
+   
+     [Fact]
+        public async Task UpdateCandidate_ReturnsBadRequestResult_WhenServiceThrowsException()
+        {
+            // Arrange
+            var candidateId = 1L;
+            var updateDto = new CandidateUpdateDto
+            {
+                FirstName = "Sardor",
+                LastName = "Murodov",
+                Email = "sardormuradov@markaz.uz"
+            };
+
+            _mockCandidateService.Setup(service => service.UpdateCandidateAsync(candidateId, updateDto))
+                                 .ThrowsAsync(new CustomException(400, "invalid_data"));
+
+            // Act
+            var result = await _controller.UpdateCandidateAsync(candidateId, updateDto);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var errorObject = Assert.IsType<ProblemDetails>(badRequestResult.Value);
+            Assert.Equal(400, errorObject.Status);
+            Assert.Equal("invalid_data", errorObject.Title);
+        }
+
+        [Fact]
+        public async Task UpdateCandidate_ReturnsNotFoundResult_WhenCandidateDoesNotExist()
+        {
+            // Arrange
+            var candidateId = 1L;
+            var updateDto = new CandidateUpdateDto
+            {
+                FirstName = "Sardor",
+                LastName = "Murodov",
+                Email = "sardormuradov@markaz.uz"
+            };
+
+            _mockCandidateService.Setup(service => service.UpdateCandidateAsync(candidateId, updateDto))
+                                 .ThrowsAsync(new CustomException(404, "candidate_not_found"));
+
+            // Act
+            var result = await _controller.UpdateCandidateAsync(candidateId, updateDto);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            var errorObject = Assert.IsType<ProblemDetails>(notFoundResult.Value);
+            Assert.Equal(404, errorObject.Status);
+            Assert.Equal("candidate_not_found", errorObject.Title);
+        }
+    
+        
+    [Fact]
+    public async Task RemoveCandidate_ReturnsNoContentResult_WhenCandidateDeleted()
+    {
+        // Arrange
+        var candidateId = 1L;
+
+        _mockCandidateService.Setup(service => service.DeleteCandidateAsync(candidateId))
+                                .ReturnsAsync(true);
+
+        // Act
+        var result = await _controller.RemoveCandidateAsync(candidateId);
+
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
+    public async Task RemoveCandidate_ReturnsNotFoundResult_WhenCandidateDoesNotExist()
+    {
+        // Arrange
+        var candidateId = 1L;
+
+        _mockCandidateService.Setup(service => service.DeleteCandidateAsync(candidateId))
+                                .ReturnsAsync(false);
+
+        // Act
+        var result = await _controller.RemoveCandidateAsync(candidateId);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+}
---
+++ CandidateHub.Tests/Services/CandidateServiceTests.cs
@@ -0,0 +1,249 @@
+﻿using AutoMapper;
+using CandidateHub.Data.Repositories;
+using CandidateHub.Domain.Entities;
+using CandidateHub.Service.DTOs.Candidate;
+using CandidateHub.Service.Exceptions;
+using CandidateHub.Service.Services;
+using Moq;
+using System.Linq.Expressions;
+
+namespace CandidateHub.Service.Tests;
+
+public class CandidateServiceTests
+{
+    private readonly Mock<IRepository<Candidate>> _mockRepository;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly CandidateService _candidateService;
+
+    public CandidateServiceTests()
+    {
+        _mockRepository = new Mock<IRepository<Candidate>>();
+        _mockMapper = new Mock<IMapper>();
+        _candidateService = new CandidateService(_mockRepository.Object, _mockMapper.Object);
+    }
+
+    [Fact]
+    public async Task CreateCandidateAsync_ShouldCreateCandidate_WhenCandidateDoesNotExist()
+    {
+        // Arrange
+        var createDto = new CandidateCreateDto
+        {
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        var candidate = new Candidate
+        {
+            Id = 3,
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync((Candidate?)null);
+
+        _mockMapper.Setup(mapper => mapper.Map<Candidate>(createDto))
+                   .Returns(candidate);
+
+        _mockRepository.Setup(repo => repo.CreateAsync(candidate))
+                       .ReturnsAsync(candidate);
+
+        _mockMapper.Setup(mapper => mapper.Map<CandidateDto>(candidate))
+                   .Returns(new CandidateDto());
+
+        // Act
+        var result = await _candidateService.CreateCandidateAsync(createDto);
+
+        // Assert
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+        _mockMapper.Verify(mapper => mapper.Map<Candidate>(createDto), Times.Once);
+        _mockRepository.Verify(repo => repo.CreateAsync(candidate), Times.Once);
+        _mockRepository.Verify(repo => repo.SaveChangesAsync(), Times.Once);
+        _mockMapper.Verify(mapper => mapper.Map<CandidateDto>(candidate), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateCandidateAsync_ShouldThrowException_WhenCandidateExists()
+    {
+        // Arrange
+        var createDto = new CandidateCreateDto
+        {
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        var existingCandidate = new Candidate
+        {
+            Id = 3,
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync(existingCandidate);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<CustomException>(async () => await _candidateService.CreateCandidateAsync(createDto));
+
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteCandidateAsync_ShouldDeleteCandidate_WhenCandidateExists()
+    {
+        // Arrange
+        var candidateId = 3L;
+
+        var candidate = new Candidate
+        {
+            Id = candidateId,
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync(candidate);
+
+        _mockRepository.Setup(repo => repo.DeleteAsync(candidateId))
+                       .ReturnsAsync(true);
+
+        // Act
+        var result = await _candidateService.DeleteCandidateAsync(candidateId);
+
+        // Assert
+        Assert.True(result);
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+        _mockRepository.Verify(repo => repo.DeleteAsync(candidateId), Times.Once);
+        _mockRepository.Verify(repo => repo.SaveChangesAsync(), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteCandidateAsync_ShouldThrowException_WhenCandidateDoesNotExist()
+    {
+        // Arrange
+        var candidateId = 3L;
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync((Candidate?)null);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<CustomException>(async () => await _candidateService.DeleteCandidateAsync(candidateId));
+
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+    }
+
+
+    [Fact]
+    public async Task GetCandidateAsync_ShouldReturnCandidate_WhenCandidateExists()
+    {
+        // Arrange
+        var candidateId = 3L;
+
+        var candidate = new Candidate
+        {
+            Id = candidateId,
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync(candidate);
+
+        _mockMapper.Setup(mapper => mapper.Map<CandidateDto>(candidate))
+                   .Returns(new CandidateDto());
+
+        // Act
+        var result = await _candidateService.GetCandidatetAsync(candidateId);
+
+        // Assert
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+        _mockMapper.Verify(mapper => mapper.Map<CandidateDto>(candidate), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetCandidateAsync_ShouldThrowException_WhenCandidateDoesNotExist()
+    {
+        // Arrange
+        var candidateId = 3L;
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync((Candidate?)null);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<CustomException>(async () => await _candidateService.GetCandidatetAsync(candidateId));
+
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateCandidateAsync_ShouldUpdateCandidate_WhenCandidateExists()
+    {
+        // Arrange
+        var candidateId = 3L;
+
+        var updateDto = new CandidateUpdateDto
+        {
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        var candidate = new Candidate
+        {
+            Id = candidateId,
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync(candidate);
+
+        _mockMapper.Setup(mapper => mapper.Map(updateDto, candidate))
+                   .Verifiable();
+
+        _mockRepository.Setup(repo => repo.Update(candidate))
+                       .Returns(candidate);
+
+        _mockMapper.Setup(mapper => mapper.Map<CandidateDto>(candidate))
+                   .Returns(new CandidateDto());
+
+        // Act
+        var result = await _candidateService.UpdateCandidateAsync(candidateId, updateDto);
+
+        // Assert
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+        _mockMapper.Verify(mapper => mapper.Map(updateDto, candidate), Times.Once);
+        _mockRepository.Verify(repo => repo.Update(candidate), Times.Once);
+        _mockRepository.Verify(repo => repo.SaveChangesAsync(), Times.Once);
+        _mockMapper.Verify(mapper => mapper.Map<CandidateDto>(candidate), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateCandidateAsync_ShouldThrowException_WhenCandidateDoesNotExist()
+    {
+        // Arrange
+        var candidateId = 3L;
+
+        var updateDto = new CandidateUpdateDto
+        {
+            FirstName = "Hurshid",
+            LastName = "Aliev",
+            Email = "hurshid@note.uz"
+        };
+
+        _mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null))
+                       .ReturnsAsync((Candidate?)null);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<CustomException>(async () => await _candidateService.UpdateCandidateAsync(candidateId, updateDto));
+
+        _mockRepository.Verify(repo => repo.GetAsync(It.IsAny<Expression<Func<Candidate, bool>>>(), true, null), Times.Once);
+    }
+}
